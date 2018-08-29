87e03e57ba40400ee7047e02a32f15d948247594
Leonardo Ochoa <leonardo@socarrao.com.br>
2018-08-29 09:58:35 -0300

    Update tests

---
+++ test/tests.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import SearchCities from '..';
+import test from 'tape';
+
+test('Should return a valid list of citis', t => {
+    let search = new SearchCities();
+    t.plan(2);
+    let result = search.search(-25.4284,-49.2733);
+    t.true(result.status);
+    t.equal(result.data[0].name,'Curitiba');
+    t.end();
+});
+
