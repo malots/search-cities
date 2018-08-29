5e0c07d1287ca78b162e4936f984c4aa99641855
Leonardo Ochoa <leonardo@socarrao.com.br>
2018-08-29 11:54:56 -0300

    Test update

--- src/SearchCities.js
+++ src/SearchCities.js
@@ -22,7 +22,6 @@ class SearchCities {
                     message: 'Not found'
                 };
             }
-            
         } else {
             return {
                 status: false,
--- test/tests.js
+++ test/tests.js
@@ -6,7 +6,7 @@ import test from 'tape';
 test('Should return a valid list of citis', t => {
     let search = new SearchCities();
     t.plan(2);
-    let result = search.search(-25.4284,-49.2733);
+    let result = search.search(-25.4284,-49.2733, 20, 2);
     t.true(result.status);
     t.equal(result.data[0].name,'Curitiba');
     t.end();
